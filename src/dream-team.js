385ca83912d38ac1e984b36c0881e29da6d0748d
drole <genuines.forever@gmail.com>
2023-12-24 19:56:28 +0400

    feat: add solution to dream-team and carbon-dating

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,20 +2,40 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members) === false) {
+    return false;
+  }
+  const newMembers = members.map((e) => {
+    if (e === null || e === undefined || typeof e === 'object') {
+      return
+    }
+    if (typeof e === 'string') {
+      return e.toUpperCase().trimStart();
+    }
+    return
+  });
+  newMembers.sort();
+  const res = [];
+  newMembers.forEach((e) => {
+    if (typeof e === 'string') {
+      let trimStr = e.trimStart();
+      res.push(trimStr.charAt(0).toUpperCase());
+      return
+    }
+  })
+  return res.join('');
 }
 
 module.exports = {
